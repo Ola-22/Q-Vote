f5e07ed3c2c7eda36a07f0086f29e71c1c3c2998
Ola-22 <olanazly@gmail.com>
2022-03-16 13:34:27 +0300

    Map Questions Data from Api, filter and sort data by total_votes

---
+++ src/Components/VotePublic/MostShared.js
@@ -0,0 +1,30 @@
+import QuestionCard from "../QuestionCard";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import SkeletonArticle from "../Skeletons/SkeletonArticle";
+
+export default function MostShared({ questions }) {
+  const MostQue = questions
+
+    ?.filter((qu) => qu.type === "public")
+    .sort((a, b) => b.total_votes - a.total_votes);
+
+  return (
+    <div>
+      {MostQue?.map((qu) => (
+        <Link to={`/vote-main/${qu.slug}`} key={qu.id}>
+          <QuestionCard
+            question={qu.question}
+            end_at={moment(qu.end_at).format("Do MMMM YYYY h a")}
+            img="/images/Group.png"
+            numberTitle="عدد المصوتين"
+            numberVote={qu.total_votes}
+            voteContent="صوت"
+          />
+        </Link>
+      ))}
+
+      {!MostQue && [1, 2, 3, 4, 5].map((n) => <SkeletonArticle key={n} />)}
+    </div>
+  );
+}
