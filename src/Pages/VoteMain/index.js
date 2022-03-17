1a34f26a147ee35f0c6976b6e2422b0f0587f5ef
Ola-22 <olanazly@gmail.com>
2022-03-17 22:01:07 +0300

    Add tabs component, useNavigate, get data questions from components

---
+++ src/Pages/VoteMain/index.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Tab from "../../Components/Tabs/Tab";
+import TabNav from "../../Components/Tabs/TabNav";
+import MostShared from "../../Components/VotePublic/MostShared";
+import NewShared from "../../Components/VotePublic/NewShared";
+import { Link, useNavigate } from "react-router-dom";
+import "./style.css";
+
+export default function VoteMain({ questions }) {
+  const [selected, setSelected] = useState("أحدث الإضافات");
+  const navigate = useNavigate();
+
+  const SelectTab = (tab) => {
+    setSelected(tab);
+  };
+
+  return (
+    <div className="vote-main">
+      <div className="headerPage">
+        <button onClick={() => navigate(-1)}>
+          <img src="/images/btnBack.png" alt="" />
+        </button>
+
+        <Link to="/" className="logo-container">
+          <img src="/images/LogoQ.png" alt="" />
+        </Link>
+      </div>
+      <TabNav
+        tabs={["أحدث الإضافات", "الأكثر مشاركة"]}
+        selected={selected}
+        SelectTab={SelectTab}
+      >
+        <>
+          <Tab isSelected={selected === "أحدث الإضافات"}>
+            <NewShared questions={questions} />
+          </Tab>
+        </>
+
+        <>
+          <Tab isSelected={selected === "الأكثر مشاركة"}>
+            <MostShared questions={questions} />
+          </Tab>
+        </>
+      </TabNav>
+    </div>
+  );
+}
