d1d3a9c51a02251fcf941cb013683066415e4d3d
Ola-22 <olanazly@gmail.com>
2022-03-19 17:40:02 +0300

    Edit props of the card

--- src/Components/ChoicesCard/index.js
+++ src/Components/ChoicesCard/index.js
@@ -1,22 +1,12 @@
 import "./style.css";
 
-export default function ChoicesCard({
-  src,
-  name,
-  onClick,
-  className,
-  progress,
-  voteNumber,
-  RateVote,
-  openModal,
-}) {
+export default function ChoicesCard({ src, name, onClick, className }) {
   return (
     <div className={className} onClick={onClick}>
       <img className="exclamation" src="/images/exclamation.png" alt="" />
       <img src={src} alt="" />
       <div className="name-content">
         <h4>{name}</h4>
-        <div>{progress}</div>
       </div>
     </div>
   );
